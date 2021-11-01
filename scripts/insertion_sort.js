d3c4d2f5602ba4f6302c6d780546cdbc239ddc76
shubham987654321 <shubhamche99@gmail.com>
2021-11-01 18:10:20 +0530

    commit change

--- scripts/insertion_sort.js
+++ scripts/insertion_sort.js
@@ -1,83 +1,42 @@
-function Heap()
+function Insertion()
 {
     c_delay=0;
 
-    heap_sort();
+    for(var j=0;j<array_size;j++)
+    {
+        div_update(divs[j],div_sizes[j],"yellow");//Color update
+
+        var key= div_sizes[j];
+        var i=j-1;
+        while(i>=0 && div_sizes[i]>key)
+        {
+            div_update(divs[i],div_sizes[i],"red");//Color update
+            div_update(divs[i+1],div_sizes[i+1],"red");//Color update
+
+            div_sizes[i+1]=div_sizes[i];
+
+            div_update(divs[i],div_sizes[i],"red");//Height update
+            div_update(divs[i+1],div_sizes[i+1],"red");//Height update
     
+            div_update(divs[i],div_sizes[i],"blue");//Color update
+            if(i==(j-1))
+            {
+                div_update(divs[i+1],div_sizes[i+1],"yellow");//Color update
+            }
+            else
+            {
+                div_update(divs[i+1],div_sizes[i+1],"blue");//Color update
+            }
+            i-=1;
+        }
+        div_sizes[i+1]=key;
+
+        for(var t=0;t<j;t++)
+        {
+            div_update(divs[t],div_sizes[t],"green");//Color update
+        }
+    }
+    div_update(divs[j-1],div_sizes[j-1],"green");//Color update
+
     enable_buttons();
-}
-
-function swap(i,j)
-{
-    div_update(divs[i],div_sizes[i],"red");//Color update
-    div_update(divs[j],div_sizes[j],"red");//Color update
-
-    var temp=div_sizes[i];
-    div_sizes[i]=div_sizes[j];
-    div_sizes[j]=temp;
-
-    div_update(divs[i],div_sizes[i],"red");//Height update
-    div_update(divs[j],div_sizes[j],"red");//Height update
-
-    div_update(divs[i],div_sizes[i],"blue");//Color update
-    div_update(divs[j],div_sizes[j],"blue");//Color update
-}
-
-function max_heapify(n,i)
-{
-    var largest=i;
-    var l=2*i+1;
-    var r=2*i+2;
-
-    if(l<n && div_sizes[l]>div_sizes[largest])
-    {
-        if(largest!=i)
-        {
-            div_update(divs[largest],div_sizes[largest],"blue");//Color update
-        }
-
-        largest=l;
-
-        div_update(divs[largest],div_sizes[largest],"red");//Color update
-    }
-
-    if(r<n && div_sizes[r]>div_sizes[largest])
-    {
-        if(largest!=i)
-        {
-            div_update(divs[largest],div_sizes[largest],"blue");//Color update
-        }
-
-        largest=r;
-
-        div_update(divs[largest],div_sizes[largest],"red");//Color update
-    }
-
-    if(largest!=i)
-    {
-        swap(i,largest);
-
-        max_heapify(n,largest);
-    }
-}
-
-function heap_sort()
-{
-    for(var i=Math.floor(array_size/2)-1;i>=0;i--)
-    {
-        max_heapify(array_size,i);
-    }
-
-    for(var i=array_size-1;i>0;i--)
-    {
-        swap(0,i);
-        div_update(divs[i],div_sizes[i],"green");//Color update
-        div_update(divs[i],div_sizes[i],"yellow");//Color update
-
-        max_heapify(i,0);
-
-        div_update(divs[i],div_sizes[i],"blue");//Color update
-        div_update(divs[i],div_sizes[i],"green");//Color update
-    }
-    div_update(divs[i],div_sizes[i],"green");//Color update
 }
