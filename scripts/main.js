500aed533a362ec0dd3a35bf2f6d64b3df139b26
Shubham Kumar <78928613+shubham987654321@users.noreply.github.com>
2021-11-01 10:41:09 +0530

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -89,3 +89,27 @@ function runalgo()
                         break;
     }
 }
+//  background color selection
+
+// background -gradinet
+
+var color1 = document.querySelector('#color1');
+var color2 = document.querySelector('#color2');
+
+var body = document.getElementById('gradient');
+
+
+function setGradient()
+{
+ 
+    body.style.backgroundImage= "linear-gradient(to right, "+ color1.value + ","+color2.value+")";
+//    document.querySelector('#hex-code').innerHTML= "color1 : "+color1.value +"<br>" +"color2 : "+color2.value
+}
+
+
+
+
+
+
+
+
